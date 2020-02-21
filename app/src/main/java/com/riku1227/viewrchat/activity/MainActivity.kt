6b728cf35d708288bd2c9daf5c787db0dae6bd18
riku1227 <17927874+riku1227@users.noreply.github.com>
2020-02-21 23:03:54 +0900

    :lipstick: Support dark theme / black theme

--- app/src/main/java/com/riku1227/viewrchat/activity/MainActivity.kt
+++ app/src/main/java/com/riku1227/viewrchat/activity/MainActivity.kt
@@ -3,11 +3,13 @@ package com.riku1227.viewrchat.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.riku1227.viewrchat.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(mainActivityToolbar)
     }
 }
