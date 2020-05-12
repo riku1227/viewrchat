594e9ddf9ee4b14d159fc1ba78f1ceb0b5ec11b8
riku1227 <17927874+riku1227@users.noreply.github.com>
2020-05-12 14:59:49 +0900

    :sparkles: Add FileUtil

---
+++ app/src/main/java/com/riku1227/viewrchat/util/FileUtil.kt
@@ -0,0 +1,32 @@
+package com.riku1227.viewrchat.util
+
+import android.content.Context
+import com.riku1227.vrchatlin.VRChatlin
+import okio.buffer
+import okio.sink
+import okio.source
+import java.io.File
+import java.nio.charset.StandardCharsets
+
+class FileUtil {
+    companion object {
+        fun <T : Any>  encodeJsonToFile(context: Context, output: File, jsonObject: T) {
+            val jsonStr = VRChatlin.get(context).getMoshi().adapter(jsonObject.javaClass).toJson(jsonObject)
+            output.sink().buffer().let {
+                it.writeString(jsonStr, StandardCharsets.UTF_8)
+                it.flush()
+                it.close()
+            }
+        }
+
+        fun <T : Any> decodeFileToJson(context: Context, input: File, jsonObject: Class<T>) : T? {
+            var jsonText: String
+            input.source().buffer().let {
+                jsonText = it.readUtf8()
+                it.close()
+            }
+
+            return VRChatlin.get(context).getMoshi().adapter(jsonObject).fromJson(jsonText)
+        }
+    }
+}
