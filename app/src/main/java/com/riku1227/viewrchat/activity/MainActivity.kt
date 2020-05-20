1b048f1288589435dbd2a168e4859c321ca3fefb
riku1227 <17927874+riku1227@users.noreply.github.com>
2020-05-21 00:43:20 +0900

    :bug: Fix not display fragment bug

--- app/src/main/java/com/riku1227/viewrchat/activity/MainActivity.kt
+++ app/src/main/java/com/riku1227/viewrchat/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.riku1227.viewrchat.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.app.AppLaunchChecker
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -83,7 +84,7 @@ class MainActivity : AppCompatActivity() {
 
             WebViewLoginActivity.REQUEST_CODE -> {
                 if(resultCode == WebViewLoginActivity.RESULT_CODE) {
-                    setupNavigation()
+                    this.recreate()
                 } else {
                     finish()
                 }
