d45f96e2c74c089cd105bc30b4c5c6a91121fb31
riku1227 <17927874+riku1227@users.noreply.github.com>
2020-05-21 15:02:39 +0900

    :sparkles: Add navigation drawer

--- app/src/main/java/com/riku1227/viewrchat/activity/MainActivity.kt
+++ app/src/main/java/com/riku1227/viewrchat/activity/MainActivity.kt
@@ -1,10 +1,21 @@
 package com.riku1227.viewrchat.activity
 
 import android.content.Intent
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.GestureDetector
+import android.view.Gravity
+import android.view.MotionEvent
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.app.AppLaunchChecker
+import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.riku1227.viewrchat.R
@@ -13,15 +24,30 @@ import com.riku1227.viewrchat.db.CacheTimeDataDB
 import com.riku1227.viewrchat.dialog.CrashReportDialog
 import com.riku1227.viewrchat.system.CacheSystem
 import com.riku1227.viewrchat.system.CrashDetection
+import com.riku1227.viewrchat.system.ErrorHandling
+import com.riku1227.viewrchat.util.VRCUtil
+import com.riku1227.viewrchat.view_model.MainActivityViewModel
 import com.riku1227.vrchatlin.VRChatlin
+import com.squareup.picasso.Picasso
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var gestureDetector: GestureDetector
+    private val compositeDisposable = CompositeDisposable()
+
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(mainActivityToolbar)
+
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
         if(savedInstanceState == null) {
             val crashDetection = CrashDetection(baseContext)
@@ -64,8 +90,34 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        gestureDetector = GestureDetector(baseContext, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onFling(
+                e1: MotionEvent?,
+                e2: MotionEvent?,
+                velocityX: Float,
+                velocityY: Float
+            ): Boolean {
+                if(e1 != null && e2 != null) {
+                    if (e2.x - e1.x > 50 && //左から右のスライド
+                        e2.y - e1.y < 50 && //上から下のスライド
+                        e1.y - e2.y < 50 && //下から上のスライド
+                        abs(velocityX) > 200 //スライド速度
+                    ) {
+                        mainActivityDrawerLayout.openDrawer(GravityCompat.START)
+                    }
+                }
+                return super.onFling(e1, e2, velocityX, velocityY)
+            }
+        })
+
+        val toggle = ActionBarDrawerToggle(this, mainActivityDrawerLayout, mainActivityToolbar, R.string.general_drawer_open, R.string.general_drawer_close)
+        mainActivityDrawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        toggle.isDrawerSlideAnimationEnabled = false
+
         if(ViewRChat.getGeneralPreferences(baseContext).getBoolean("is_login", false)) {
             setupNavigation()
+            setupDrawer()
         }
     }
 
@@ -92,11 +144,93 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        gestureDetector.onTouchEvent(ev)
+        return super.dispatchTouchEvent(ev)
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
+
     private fun setupNavigation() {
         val navHost = mainActivityFragment as NavHostFragment
         val navController = navHost.navController
         val graph = navController.navInflater.inflate(R.navigation.activity_main_navigation)
         navController.graph = graph
         NavigationUI.setupWithNavController(mainActivityBottomNavigation, navController)
+    }
+
+    private fun setupDrawer() {
+        val headerView = mainActivityNavigationDrawer.getHeaderView(0)
+
+        if(viewModel.loginUser == null) {
+            val disposable = VRChatlin.get(applicationContext).APIService().getCurrentUserInfo()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    {
+                        viewModel.loginUser = it
+                        headerView.findViewById<ImageView>(R.id.drawerMainActivityUserThumbnail).outlineProvider = ViewRChat.imageRadiusOutlineProvider
+                        val disposable02 = CacheSystem.loadImage(applicationContext, CacheSystem.CacheType.USER_AVATAR_IMAGE, it.id, it.currentAvatarImageUrl)
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(
+                                { imgFile ->
+                                    Picasso.get()
+                                        .load(imgFile)
+                                        .centerCrop()
+                                        .fit()
+                                        .into(
+                                            headerView.findViewById<ImageView>(R.id.drawerMainActivityUserThumbnail)
+                                        )
+                                },
+                                {
+                                    ErrorHandling.onNetworkError(it, applicationContext, activity = this)
+                                }
+                            )
+                        compositeDisposable.add(disposable02)
+
+                        headerView.findViewById<TextView>(R.id.drawerMainActivityUserName).text = it.displayName
+                        headerView.findViewById<TextView>(R.id.drawerMainActivityUserRank).text = VRCUtil.getTrustRank(it.tags)
+                        headerView.findViewById<TextView>(R.id.drawerMainActivityPlatform).text = VRCUtil.getLastLoginPlatform(it.last_platform)
+                        headerView.findViewById<TextView>(R.id.drawerMainActivityFriendsCount).text = it.friends?.size.toString()
+                    },
+                    {
+                        ErrorHandling.onNetworkError(it, applicationContext, activity = this)
+                    }
+                )
+            compositeDisposable.add(disposable)
+        } else {
+            viewModel.loginUser?.let {
+                headerView.findViewById<ImageView>(R.id.drawerMainActivityUserThumbnail).outlineProvider = ViewRChat.imageRadiusOutlineProvider
+
+                val disposable02 = CacheSystem.loadImage(applicationContext, CacheSystem.CacheType.USER_AVATAR_IMAGE, it.id, it.currentAvatarImageUrl)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                        { imgFile ->
+                            Picasso.get()
+                                .load(imgFile)
+                                .centerCrop()
+                                .fit()
+                                .into(
+                                    headerView.findViewById<ImageView>(R.id.drawerMainActivityUserThumbnail)
+                                )
+                        },
+                        {
+                            ErrorHandling.onNetworkError(it, applicationContext, activity = this)
+                        }
+                    )
+                compositeDisposable.add(disposable02)
+
+                headerView.findViewById<TextView>(R.id.drawerMainActivityUserName).text = it.displayName
+                headerView.findViewById<TextView>(R.id.drawerMainActivityUserRank).text = VRCUtil.getTrustRank(it.tags)
+                headerView.findViewById<TextView>(R.id.drawerMainActivityPlatform).text = VRCUtil.getLastLoginPlatform(it.last_platform)
+                headerView.findViewById<TextView>(R.id.drawerMainActivityFriendsCount).text = it.friends?.size.toString()
+            }
+        }
+
     }
 }
